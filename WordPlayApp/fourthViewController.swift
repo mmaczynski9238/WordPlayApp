5bff587b775150ee9f94acfdf5d0d47523c51555
student3 <student3@jh-mbair-158-17.d214.org>
2015-12-04 14:48:34 -0600

    Started creating ViewControllers

---
+++ WordPlayApp/fourthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  fourthViewController.swift
+//  WordPlayApp
+//
+//  Created by student3 on 12/4/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class fourthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
